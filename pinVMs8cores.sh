4a87a8bed3f35d1f4fb6920be2542836584b0878
Jakub Krzywda <jakub@cs.umu.se>
2016-04-02 17:35:02 +0200

    Added files via upload

---
+++ pinVMs8cores.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+virsh vcpupin WikiImage_German35 0 0
+virsh vcpupin WikiImage_German35 1 1
+virsh vcpupin WikiImage_German35 2 2
+virsh vcpupin WikiImage_German35 3 3
+virsh vcpupin WikiImage_German35 4 4
+virsh vcpupin WikiImage_German35 5 5
+virsh vcpupin WikiImage_German35 6 6
+virsh vcpupin WikiImage_German35 7 7
+
+virsh vcpupin WikiImage_German34 0 8
+virsh vcpupin WikiImage_German34 1 9
+virsh vcpupin WikiImage_German34 2 10
+virsh vcpupin WikiImage_German34 3 11
+virsh vcpupin WikiImage_German34 4 12
+virsh vcpupin WikiImage_German34 5 13
+virsh vcpupin WikiImage_German34 6 14
+virsh vcpupin WikiImage_German34 7 15
+
+virsh vcpupin WikiImage_German33 0 16
+virsh vcpupin WikiImage_German33 1 17
+virsh vcpupin WikiImage_German33 2 18
+virsh vcpupin WikiImage_German33 3 19
+virsh vcpupin WikiImage_German33 4 20
+virsh vcpupin WikiImage_German33 5 21
+virsh vcpupin WikiImage_German33 6 22
+virsh vcpupin WikiImage_German33 7 23
+
+virsh vcpupin WikiImage_German31 0 24
+virsh vcpupin WikiImage_German31 1 25
+virsh vcpupin WikiImage_German31 2 26
+virsh vcpupin WikiImage_German31 3 27
+virsh vcpupin WikiImage_German31 4 28
+virsh vcpupin WikiImage_German31 5 29
+virsh vcpupin WikiImage_German31 6 30
+virsh vcpupin WikiImage_German31 7 31
---
+++ unpinVMs8cores.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+virsh vcpupin WikiImage_German35 0 0-31
+virsh vcpupin WikiImage_German35 1 0-31
+virsh vcpupin WikiImage_German35 2 0-31
+virsh vcpupin WikiImage_German35 3 0-31
+virsh vcpupin WikiImage_German35 4 0-31
+virsh vcpupin WikiImage_German35 5 0-31
+virsh vcpupin WikiImage_German35 6 0-31
+virsh vcpupin WikiImage_German35 7 0-31
+
+virsh vcpupin WikiImage_German34 0 0-31
+virsh vcpupin WikiImage_German34 1 0-31
+virsh vcpupin WikiImage_German34 2 0-31
+virsh vcpupin WikiImage_German34 3 0-31
+virsh vcpupin WikiImage_German34 4 0-31
+virsh vcpupin WikiImage_German34 5 0-31
+virsh vcpupin WikiImage_German34 6 0-31
+virsh vcpupin WikiImage_German34 7 0-31
+
+virsh vcpupin WikiImage_German33 0 0-31
+virsh vcpupin WikiImage_German33 1 0-31
+virsh vcpupin WikiImage_German33 2 0-31
+virsh vcpupin WikiImage_German33 3 0-31
+virsh vcpupin WikiImage_German33 4 0-31
+virsh vcpupin WikiImage_German33 5 0-31
+virsh vcpupin WikiImage_German33 6 0-31
+virsh vcpupin WikiImage_German33 7 0-31
+
+virsh vcpupin WikiImage_German31 0 0-31
+virsh vcpupin WikiImage_German31 1 0-31
+virsh vcpupin WikiImage_German31 2 0-31
+virsh vcpupin WikiImage_German31 3 0-31
+virsh vcpupin WikiImage_German31 4 0-31
+virsh vcpupin WikiImage_German31 5 0-31
+virsh vcpupin WikiImage_German31 6 0-31
+virsh vcpupin WikiImage_German31 7 0-31
