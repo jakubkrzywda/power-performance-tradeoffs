23a6a1c1ce402cc6755a498957970a08b6d4c528
Jakub Krzywda <jakub@cs.umu.se>
2016-04-04 13:49:33 +0200

    Update horizontal_scaling_single_experiment.sh

--- horizontal_scaling_single_experiment.sh
+++ horizontal_scaling_single_experiment.sh
@@ -9,10 +9,12 @@ host= # address of the physical machine hosting virtual machines
 user= # user at the physical machine with permissions to
 power_measurement_server= # address of the physical machine providing the power consumption measurements
 host_address= # address for measuring the power consumption of the host
-vms=1 # number of virtual machines
+vms=4 # number of virtual machines
 frequency=2.1 # cpu frequency
 measurement_duration=440 # measurement duration [in seconds]: 40 [steps/levels] * 10 [repetitions at each step/level] + 40 [safe margin]
-node=p05 # name of physical machine for logging purposes
+node= # name of physical machine for logging purposes
+results_directory=tests # directory to store experiment results
+results_filename_prefix=horizontal-scaling # directory to store experiment results
         
 while [ "$1" != "" ]; do
     case $1 in
@@ -43,9 +45,9 @@ python WikipediaGenerator.py -p evenly_spread -m 200 -s 5 -r 10 -w 200 &
 start_time=$SECONDS
 while [ $SECONDS -lt $((start_time + measurement_duration)) ]
 do
-  snmpget -c public -v 2c $power_measurement_server $host_address | awk -v pm="$node" -v freq="$freq_m" -v vms="$vms" -v time="$((SECONDS - beginning))" -v percentage="$percentage" -v iteration="$i" -v temperature="$temperature" '//{print pm";"freq";"vms";"$4";"time";"percentage";"iteration temperature}' >> tests/dvfs-power-performance_${vms}_${frequency}_${experiment_id}.csv
+  snmpget -c public -v 2c $power_measurement_server $host_address | awk -v pm="$node" -v freq="$freq_m" -v vms="$vms" -v time="$((SECONDS - beginning))" -v percentage="$percentage" -v iteration="$i" -v temperature="$temperature" '//{print pm";"freq";"vms";"$4";"time";"percentage";"iteration temperature}' >> ${results_directory}//${results_filename_prefix}_${vms}_${frequency}_${experiment_id}.csv
   sleep 1
 done
 
 # save temporary log file
-mv MultiProcessedResponseTime.out tests/horizontal-scaling-power-performance_${vms}_${frequency}_${experiment_id}_MultiProcessedResponseTime.out
+mv MultiProcessedResponseTime.out ${results_directory}/${results_filename_prefix}_${vms}_${frequency}_${experiment_id}_MultiProcessedResponseTime.out
